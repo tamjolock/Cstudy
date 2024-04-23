05bf933899f060b1750bc738143759a84a7fa271
hiljo <voreja@naver.com>
2024-04-24 05:48:50 +0900

    포인터

--- Az trans.cpp
+++ Az trans.cpp
@@ -1,29 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-
-
-int main(void)
-{
-	int count=0, i,size;
-	char str[50];
-
-	printf("문장을 입력 : ");
-	fgets(str,sizeof(str),stdin);
-	
-	for (i = 0;i < sizeof(str); i++)
-	{
-
-		if ('A' <= str[i] && str[i] <= 'Z')
-		{
-			str[i] += 32;
-			count += 1;
-		}
-
-	}
-	
-	printf("바뀐 문장 : %s바뀐 문자 수 : %d", str,count);
-	
-	
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//
+//
+//int main(void)
+//{
+//	int count=0, i;
+//	char str[50];
+//
+//	printf("문장을 입력 : ");
+//	fgets(str,sizeof(str),stdin);
+//	
+//	for (i = 0;i < sizeof(str); i++)
+//	{
+//
+//		if ('A' <= str[i] && str[i] <= 'Z')
+//		{
+//			str[i] += 32;
+//			count += 1;
+//		}
+//
+//	}
+//	
+//	printf("바뀐 문장 : %s바뀐 문자 수 : %d", str,count);
+//	
+//	
+//	return 0;
+//}
